f841d6c2e3b543b94e4b78ab172c0ee5764d3f6b
Ikeoluwa Kolawole <kolawoleikeoluwa2018@gmail.com>
2024-04-17 06:10:44 +0100

    dynamic subnetting

--- terraform-aws-k3s/networking/main.tf
+++ terraform-aws-k3s/networking/main.tf
@@ -1,8 +1,17 @@
 # --- networking/main.tf ---
 
+data "aws_availability_zones" "available" {}
+
+
+
 resource "random_integer" "random" {
   min = 1
   max = 100
+}
+
+resource "random_shuffle" "az_list" {
+  input        = data.aws_availability_zones.available.names
+  result_count = var.max_subnets
 }
 
 resource "aws_vpc" "vtl_vpc" {
@@ -16,11 +25,11 @@ resource "aws_vpc" "vtl_vpc" {
 }
 
 resource "aws_subnet" "vtl_public_subnet" {
-  count                   = length(var.public_cidrs)
+  count                   = var.public_sn_count
   vpc_id                  = aws_vpc.vtl_vpc.id
   cidr_block              = var.public_cidrs[count.index]
   map_public_ip_on_launch = true
-  availability_zone       = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"][count.index]
+  availability_zone       = random_shuffle.az_list.result[count.index]
 
   tags = {
     Name = "vtl_public_${count.index + 1}"
@@ -28,11 +37,11 @@ resource "aws_subnet" "vtl_public_subnet" {
 }
 
 resource "aws_subnet" "vtl_private_subnet" {
-  count                   = length(var.private_cidrs)
+  count                   = var.private_sn_count
   vpc_id                  = aws_vpc.vtl_vpc.id
   cidr_block              = var.private_cidrs[count.index]
   map_public_ip_on_launch = false
-  availability_zone       = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"][count.index]
+  availability_zone       = random_shuffle.az_list.result[count.index]
 
   tags = {
     Name = "vtl_private_${count.index + 1}"
